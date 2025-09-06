632e37529190d223dfb7f82926b109511a795315
MD Arafat Alam Leom <mdarafatalamleon10@gmail.com>
2025-09-06 11:06:21 +0600

    added upto nav bar

---
+++ script.js
@@ -0,0 +1,21 @@
+//loginbutton functionality
+ document.getElementById("loginButton").addEventListener("click",function(e){ 
+    e.preventDefault();
+   const mobileNumber=12345;
+   const pinNumber=123;
+
+   const mobileNumberValue=document.getElementById("mobile-number").value;
+const mobileNumberValueConverted=parseInt(mobileNumberValue);
+
+const pinNumberValue=document.getElementById("pin-number").value;
+ const pinNumberValueConverted=parseInt(pinNumberValue);
+
+ console.log(mobileNumberValueConverted,pinNumberValueConverted);
+
+ if(mobileNumber===mobileNumberValueConverted && pinNumber=== pinNumberValueConverted){
+   window.location.href="./home.html"
+ }else{
+    alert("Invalid Credentials");
+ }
+ })
+
